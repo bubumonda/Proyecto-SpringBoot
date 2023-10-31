6f8eebb5ebc38c0c42f273a9b1a623f28b3a22dc
Gian Mier <gian2210073@correo.uis.edu.co>
2023-10-30 22:15:27 -0500

    Login Spring Security-JWT

---
+++ src/main/java/com/uis/entornos/proyectologincrud/ProyectoLoginCrudApplication.java
@@ -0,0 +1,13 @@
+package com.uis.entornos.proyectologincrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLoginCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoLoginCrudApplication.class, args);
+    }
+
+}
